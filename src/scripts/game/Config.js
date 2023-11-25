a84c4d5942281484d9e4452c80bb87625c4f7d91
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-11-25 15:26:38 +0400

    Creating multiple platforms

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -3,10 +3,26 @@ import { Tools } from "../system/Tools";
 
 export const Config = {
     bgSpeed: 2,
+    platforms: {
+        ranges: {
+            rows: {
+                min: 2,
+                max: 6
+            },
+            cols: {
+                min: 3,
+                max: 9
+            },
+            offset: {
+                min: 60,
+                max: 200
+            }
+        }
+    },
     hero: {
         position: {
             x: 350,
-            y: 595
+            y: 395
         }
     },
     loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)),
--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -2,25 +2,21 @@ import { App } from "../system/App";
 import { Scene } from "../system/Scene";
 import { Background } from "./Background";
 import { Hero } from "./Hero";
-import { Platform } from "./Platform";
+import { Platforms } from "./Platforms";
 
 export class Game extends Scene {
     create() {
         this.createBackground();
-        this.createPlatform({
-            rows: 4,
-            cols: 6,
-            x: 200
-        });
         this.createHero();
+        this.createPlatforms();
+    }
+    createPlatforms() {
+        this.platfroms = new Platforms();
+        this.container.addChild(this.platfroms.container);
     }
     createHero() {
         this.hero = new Hero();
         this.container.addChild(this.hero.sprite);
-    }
-    createPlatform(data) {
-        const platform = new Platform(data.rows, data.cols, data.x);
-        this.container.addChild(platform.container);
     }
     createBackground() {
         this.bg = new Background();
@@ -28,5 +24,6 @@ export class Game extends Scene {
     }
     update(dt) {
         this.bg.update(dt);
+        this.platfroms.update(dt);
     }
 }
